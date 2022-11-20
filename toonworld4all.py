22d070f9ee46515d77a39978accc9ab56beda452
Jack <92579700+JohnWickKeanue@users.noreply.github.com>
2022-11-20 16:58:02 +0530

    Create toonworld4all.py

---
+++ toonworld4all.py
@@ -0,0 +1,39 @@
+import time
+from bs4 import BeautifulSoup
+import requests
+from requests import get
+url = "https://toonworld4all.me/teen-titans-trouble-in-tokyo-2006-bluray-multi-audio-hindi/"
+
+def flix(url):
+    client = requests.session()
+    r = client.get(url).text
+    soup = BeautifulSoup (r, "html.parser")
+    for a in soup.find_all("a"):
+                   c= a.get("href")
+                   if "redirect/main.php?" in c:
+                       download = get(c, stream=True, allow_redirects=False)
+                       v = download.headers["location"]
+                       DOMAIN = "https://blog.disheye.com"
+                       code = v.split("/")[-1]
+                       final_url = f"{DOMAIN}/{code}?quelle="
+                       resp = client.get(final_url)
+                       soup = BeautifulSoup(resp.content, "html.parser")
+    
+                       try: inputs = soup.find(id="go-link").find_all(name="input")
+                       except: return "Incorrect Link"
+    
+                       data = { input.get('name'): input.get('value') for input in inputs }
+
+                       h = { "x-requested-with": "XMLHttpRequest" }
+    
+                       time.sleep(10)
+                       r = client.post(f"{DOMAIN}/links/go", data=data, headers=h)
+                       g = r.json()['url']
+                       if "gdtot" in g:
+                           t = client.get(g).text
+                           soupt = BeautifulSoup(t, "html.parser")
+                           title = soupt.title
+                           gd_txt = f"{(title.text).replace('GDToT | ' , '')}\n{g}\n\n"
+                           print(gd_txt)
+
+print(flix(url))
